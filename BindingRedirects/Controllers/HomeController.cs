4d2f81135e42d0b0c162a15d85bbf5bf9a14a26f
Ken Schlobohm <keschlob@microsoft.com>
2021-07-12 14:13:28 -0500

    now connecting to Key Vault

--- BindingRedirects/Controllers/HomeController.cs
+++ BindingRedirects/Controllers/HomeController.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace BindingRedirects.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index(CancellationToken token)
         {
             ViewBag.Title = "Home Page";
+
+            var secretService = new SecretServices.SecretService();
+            var connectionString = await secretService.GetSecretForFileServiceAsync(token);
+
+            ViewBag.ConnectionString = connectionString;
 
             return View();
         }
